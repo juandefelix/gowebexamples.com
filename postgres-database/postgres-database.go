b501a0b9677baa53bc004d41c00aa8e63abb9ba3
Juan Ortiz <juanfortizd@gmail.com>
2023-12-13 11:40:01 -0600

    read and traverse over users in database

--- postgres-database/postgres-database.go
+++ postgres-database/postgres-database.go
@@ -2,23 +2,50 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"log"
 	"database/sql"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	connStr := "postgres://juanortiz@localhost/standard_communities_dev?sslmode=disable"
+	connStr := "postgres://user:password@localhost/test_database"
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		fmt.Println("Error connecing to the database:", err)
+		log.Fatal(err)
 	}
 
-	rows, err := db.Query("SELECT email FROM admin_users")
-	if err != nil {
-		fmt.Println("Error reading database:", err)
+	type user struct {
+		id int
+		firstName string
+		lastName string
+		email string
+		createdAt time.Time
 	}
 
-	fmt.Println("Results:")
-	fmt.Println(rows.Columns())
+
+	{ // Query all users
+		rows, err := db.Query(`SELECT id, first_name, last_name, email, created_at FROM admin_users`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		var users []user
+		for rows.Next() {
+			var u user
+			err = rows.Scan(&u.id, &u.firstName, &u.lastName, &u.email, &u.createdAt)
+			if err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, u)
+		}
+
+		fmt.Printf("%#v", users)
+
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
