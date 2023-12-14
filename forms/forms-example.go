a3e0b6a38e640b24c19b2da22397df14b377fc3d
Juan Ortiz <juanfortizd@gmail.com>
2023-12-14 13:33:03 -0600

    add logging middleware to forms tutorial

--- forms/forms-example.go
+++ forms/forms-example.go
@@ -4,7 +4,6 @@ import (
 	"html/template"
 	"net/http"
 	"log"
-	"fmt"
 )
 
 type ContactDetails struct {
@@ -13,27 +12,36 @@ type ContactDetails struct {
 	Message string
 }
 
+func logging(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Print(r.URL.Path)
+		f(w,r)
+	}
+}
+
+func rootPathHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("forms.html"))
+
+	if r.Method != http.MethodPost {
+		tmpl.Execute(w, nil)
+		return
+	}
+
+	details := ContactDetails{
+		Email: r.FormValue("email"),
+		Subject: r.FormValue("subject"),
+		Message: r.FormValue("message"),
+	}
+	log.Println("Form values: ", details.Email, details.Subject, details.Message)
+
+	tmpl.Execute(w, struct{ Success bool }{true})
+}
+
 func main() {
 	// The program will try to find the template in the same directory where you run the `go run command,`
 	// so make sure that you run this file from the same directory in your terminal
-	tmpl := template.Must(template.ParseFiles("forms.html"))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
-			return
-		}
-
-		details := ContactDetails{
-			Email: r.FormValue("email"),
-			Subject: r.FormValue("subject"),
-			Message: r.FormValue("message"),
-		}
-		fmt.Println("here")
-		log.Print("%v %v %v", details.Email, details.Subject, details.Message)
-
-		tmpl.Execute(w, struct{ Success bool }{true})
-	})
+	http.HandleFunc("/", logging(rootPathHandler))
 
 	http.ListenAndServe(":8080", nil)
 }
