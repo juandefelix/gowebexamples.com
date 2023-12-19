9bf167b60176819d0df11bd4336717024382c9a2
Juan Ortiz <juanfortizd@gmail.com>
2023-12-18 21:55:13 -0600

    add first middleware

---
+++ middleware-advanced/middleware-advanced.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	// "log"
+	// "time"
+	"net/http"
+)
+
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+
+func Chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc{
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	return f
+}
+
+func Logging() Middleware {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			defer func() { log.Println(r.URL.Path, time.Since(start))}
+			f(w, r)
+		}
+	}
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello world")
+}
+
+func main() {
+	http.HandleFunc("/", Chain(Hello, Logging()))
+	http.ListenAndServe(":8080", nil)
+}
+
+// what's the signature of the handler function?
+// 	- a function that accepts a ResponseWriter and a Request
+// what's the signature of the Middleware?
+// 	- a function that accetsa a handler function and returns another
+
